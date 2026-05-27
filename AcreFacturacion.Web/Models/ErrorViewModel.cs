1cdd0e80f759ac338fc434cee65e4576966e48eb
jolopez <jolopez@azzam.es>
2026-05-27 10:18:40 +0200

    Inicializa solucion ASP.NET Core MVC

---
+++ AcreFacturacion.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AcreFacturacion.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
